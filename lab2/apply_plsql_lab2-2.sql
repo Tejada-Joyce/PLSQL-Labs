2f1136c5f4b235fbcde91a415b51e0d8c39dfeeb
Joyce Tejada <joyce_105_3@hotmail.com>
2020-01-17 19:57:40 -0700

    Lab 2 Part 2

--- lab2/apply_plsql_lab2-2.sql
+++ lab2/apply_plsql_lab2-2.sql
@@ -5,13 +5,35 @@
 */
 
 -- Call seeding libraries.
-@$LIB/cleanup_oracle.sql
-@$LIB/Oracle12cPLSQLCode/Introduction/create_video_store.sql
+--@/home/student/Data/cit325/lib/cleanup_oracle.sql
+--@/home/student/Data/cit325/lib/Oracle12cPLSQLCode/Introduction/create_video_store.sql
 
 -- Open log file.
-SPOOL apply_plsql_lab2-2.txt
+--SPOOL apply_plsql_lab2-2.txt
+
+SET SERVEROUTPUT ON
+SET VERIFY OFF
 
 -- Enter your solution here.
+DECLARE
+    lv_input        VARCHAR2(10);
+    lv_raw_input    VARCHAR2(20);
+BEGIN
+    lv_raw_input := NVL('&1', NULL);
+    
+    IF LENGTH(lv_raw_input) <= 10 THEN
+        dbms_output.put_line('Hello '||lv_raw_input||'!');
+    ELSIF LENGTH(lv_raw_input) > 10 THEN
+        lv_input := SUBSTR(lv_raw_input, 1, 10);
+        dbms_output.put_line('Hello '||lv_input||'!');
+    ELSE
+        dbms_output.put_line('Hello World!');
+    END IF;
+END;
+/
+
 
 -- Close log file.
-SPOOL OFF
+--SPOOL OFF
+
+QUIT;
